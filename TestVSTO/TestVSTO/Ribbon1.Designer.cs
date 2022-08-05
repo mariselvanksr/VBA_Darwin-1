2af72aa411a548fe099586e098be02d55c4268c9
Mariselvan <mariselvan.murugasan@zoomrx.com>
2022-08-05 17:18:17 +0530

    Update

--- TestVSTO/TestVSTO/Ribbon1.Designer.cs
+++ TestVSTO/TestVSTO/Ribbon1.Designer.cs
@@ -63,7 +63,7 @@
             // 
             // button1
             // 
-            this.button1.Label = "button 2";
+            this.button1.Label = "button 3";
             this.button1.Name = "button1";
             this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
             // 
